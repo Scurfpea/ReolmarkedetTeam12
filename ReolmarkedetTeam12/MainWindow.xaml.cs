97968cd610f88dbfc02a658ea615667b96209002
Maja Daniela Hansen <mdha54168.ucl.dk>
2023-09-24 11:08:54 +0200

    Saving user input as the correct type.

--- ReolmarkedetTeam12/MainWindow.xaml.cs
+++ ReolmarkedetTeam12/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Security.Policy;
 using System.Text;
@@ -24,16 +25,43 @@ namespace ReolmarkedetTeam12
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        bool CorrectPhone, CorrectDate, CorrectWeeks;
         string name, email, phone, date, weeks;
+        int phoneInt, weeksInt;
+        DateTime dateDateTime;
+
+
+        private void Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Phone_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Date_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Weeks_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-        }
+      
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -43,7 +71,51 @@ namespace ReolmarkedetTeam12
             date = DateTxt.Text;
             weeks = WeeksTxt.Text;
 
-            Debug.WriteLine(@"{0}, {1}, {2} {3} {4}",name, email, phone, date, weeks); //check if it works.
+            if (Int32.TryParse(phone, out int phoneInt))
+            {
+                Debug.WriteLine($"Was able to parse '{phone}'");
+                CorrectPhone = true;               
+            }
+            else
+            {
+                Debug.WriteLine($"Unable to parse '{phone}'");
+                //Show dialog
+                MessageBox.Show("Ikke korrekt værdi. Prøv igen");
+                PhoneTxt.Text = ""; //Clear the error.
+            }
+
+            if (Int32.TryParse(weeks, out int weeksInt))
+            {
+                Debug.WriteLine($"Was able to parse '{weeks}'");
+                CorrectWeeks = true;
+            }
+            else
+            {
+                Debug.WriteLine($"Unable to parse '{weeks}'");
+                //Show dialog
+                MessageBox.Show("Ikke korrekt værdi. Prøv igen");
+                WeeksTxt.Text = ""; //Clear the error.
+            }
+
+            var dateDateTimeVar = DateTime.Parse(date, new CultureInfo("en-US", true)); //'MM/dd/yyyy' forma
+            dateDateTime = dateDateTimeVar;
+
+            //if (!DateTime.TryParse(date, out dateDateTime))
+            //{
+            //    MessageBox.Show("Ikke korrekt værdi. Prøv igen");
+            //}
+
+
+            if (CorrectPhone && CorrectWeeks && name != null && email != null && date != null) //missing proper date check
+            {
+                CreateNewRenter();
+            }
+
+        }
+        void CreateNewRenter()
+        {            
+            Debug.WriteLine(@"{0}, {1}, {2} {3} {4}", name, email, phoneInt, dateDateTime, weeks); //check if it works.
+            MessageBox.Show("Din booking er godkendt");
         }
     }
 }
